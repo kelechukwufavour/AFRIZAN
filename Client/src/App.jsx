0e4cdf169f35b01d8645578b50012c17cc3bf091
kelechukwufavour <kelechukwufavour.faye@gmail.com>
2024-06-20 17:20:09 +0100

    installed dependencies, updated readme

---
+++ Client/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+//import Main from './components/Main';
+//import About from './components/About';
+//import SignInForm from './components/SignInForm'; 
+//import SignUpForm from './components/SignUpForm';
+//import ForgotPassword from './components/ForgotPassword';
+
+
+import './App.css';
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/signin" element={<SignInForm />} />
+            <Route path="/signup" element={<SignUpForm />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} /> {/* Added route */}
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # AFRIZAN
+Discover the Soul of African Artistry
+Product Overview
+AFRIZAN is an online platform that connects African artisans with global consumers, offering a marketplace for handmade, culturally significant products. Our goal is to empower artisans by providing visibility and a means to reach a wider audience.
+Target Audience
+Primary: Global consumers interested in unique and handmade items.
+Secondary: Collectors, art enthusiasts, and those interested in ethical and sustainable shopping.
+Unique Selling Proposition
+AFRIZAN differentiates itself by integrating cultural storytelling with product listings, creating an emotional connection between consumers and the artisans’ heritage.
